726c8d1766062ba83355d01acf4addf273b3f679
Nakatai-0322 <nakatai0322@gmail.com>
2022-01-16 13:13:07 +0900

    Add Change Language

---
+++ index.js
@@ -0,0 +1,16 @@
+function BirJapanese() {
+    const BirJpn = "3月22日"
+    document.getElementById("birstr").innerHTML = BirJpn;
+}
+function BirEnglish() {
+    const BirEng = "March 22"
+    document.getElementById("birstr").innerHTML = BirEng;
+}
+function BirChinese() {
+    const BirChi = "3月22日"
+    document.getElementById("birstr").innerHTML = BirChi;
+}
+function BirKorean() {
+    const BirKor = "3월 22일"
+    document.getElementById("birstr").innerHTML = BirKor;
+}
