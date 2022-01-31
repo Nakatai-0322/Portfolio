f83fbed20706aca332021646bbee9c8ea2e79956
Nakatai <info@nakatai.ga>
2022-01-31 10:22:34 +0900

    Update birthday.js

--- assets/js/birthday.js
+++ assets/js/birthday.js
@@ -1,14 +1,14 @@
 const birHTML = "birstr";
 
 function BirJapanese() {
-    document.getElementById.birHTML.innerHTML = "3月22日";
+    document.getElementById(birHTML).innerHTML = "3月22日";
 }
 function BirEnglish() {
-    document.getElementById.birHTML.innerHTML = "March 22";
+    document.getElementById(birHTML).innerHTML = "March 22";
 }
 function BirChinese() {
-    document.getElementById.birHTML.innerHTML = "3月22日";
+    document.getElementById(birHTML).innerHTML = "3月22日";
 }
 function BirKorean() {
-    document.getElementById.birHTML.innerHTML = "3월 22일";
+    document.getElementById(birHTML).innerHTML = "3월 22일";
 }
