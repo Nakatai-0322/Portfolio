5e5ba66d7dd505b28969fe5568335c096a921cc4
Nakatai-0322 <nakatai0322@gmail.com>
2022-03-31 08:34:56 +0900

    Update birthday.js

--- assets/js/birthday.js
+++ assets/js/birthday.js
@@ -2,16 +2,16 @@ const birHTML = "birstr";
 
 function BirJapanese() {
     document.getElementById(birHTML).innerHTML = "3月22日";
-}
+};
 
 function BirEnglish() {
     document.getElementById(birHTML).innerHTML = "March 22";
-}
+};
 
 function BirChinese() {
     document.getElementById(birHTML).innerHTML = "3月22日";
-}
+};
 
 function BirKorean() {
     document.getElementById(birHTML).innerHTML = "3월 22일";
-}
+};
