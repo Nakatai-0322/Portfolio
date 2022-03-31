2bd99a7fa4628687757e4113a40a669dd65fb498
Nakatai-0322 <nakatai0322@gmail.com>
2022-04-01 07:42:30 +0900

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -199,14 +199,14 @@
     });
 
     /*
-    Initiate portfolio lightbox 
+    Initiate portfolio lightbox
     */
     const portfolioLightbox = GLightbox({
         selector: '.portfolio-lightbox'
     });
 
     /*
-    Initiate portfolio details lightbox 
+    Initiate portfolio details lightbox
     */
     const portfolioDetailsLightbox = GLightbox({
         selector: '.portfolio-details-lightbox',
