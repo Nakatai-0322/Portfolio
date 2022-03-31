666d95650c03c4f7bf24ab552b50992b66cd6aad
Nakatai-0322 <nakatai0322@gmail.com>
2022-04-01 07:40:47 +0900

    Update days.js

--- assets/js/days.js
+++ assets/js/days.js
@@ -2,7 +2,6 @@ let from;
 let to;
 let ms;
 let days;
-let _years;
 let years;
 
 window.onload = function(){
@@ -10,8 +9,7 @@ window.onload = function(){
     to = new Date();
     ms = to.getTime() - from.getTime();
     days = Math.floor(ms / (1000*60*60*24));
-    _years = (days / 365);
-    years = _years.toFixed(1);
+    years = (days / 365).toFixed(1);
 
     document.getElementById("days").innerHTML = days;
     document.getElementById("years").innerHTML = years;
