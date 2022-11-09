e617ee081814a1b25a5dc5747fb5e42981132ce4
Nakatai-0322 <nakatai0322@gmail.com>
2022-11-09 18:57:47 +0900

    Update index.js

--- src/router/index.js
+++ src/router/index.js
@@ -4,7 +4,7 @@ import routes from "./routes";
 const router = vueRouter.createRouter({
 	history: vueRouter.createWebHistory(import.meta.env.BASE_URL),
 	base: import.meta.env.BASE_URL,
-	routes
+	routes,
 });
 
 export default router;
