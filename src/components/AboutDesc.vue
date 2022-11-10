424415883fe4ffa0b65e2366815c223ff8519619
Nakatai-0322 <nakatai0322@gmail.com>
2022-11-10 20:40:38 +0900

    Update AboutDesc.vue

--- src/components/AboutDesc.vue
+++ src/components/AboutDesc.vue
@@ -3,6 +3,25 @@
 		私は、"Nakatai"（なかたい）といいます。<br>
 		岐阜県出身で、現在居住しています。<br>
 		年齢は██歳、誕生日は3月22日です。
+		<h1>Who am I?</h1>
+		<p>
+			<img src="https://cdn.nakatai.ga/img/nakatai.webp" width="300px"><br>
+			Nakatai
+		</p>
+		<h2>コメント</h2>
+		<p>
+
+		</p>
+		<h2>BASE Data</h2>
+		<p>
+			誕生日: ████-03-22<br>
+			趣味: プログラミング, ゲーム<br>
+		</p>
+		<h2>WORKS</h2>
+		<p>
+			今の仕事: 非公開（非IT）
+			Web活動など: <a href="https://go.nakatai.ga/orgs">こちら</a>をご参照ください。
+		</p>
 	</div>
 </template>
 
@@ -15,5 +34,10 @@ export default {
 <style scoped>
 .desp {
 	color: white;
+}
+
+h2 {
+	font-size: 2rem;
+	font-weight: 700;
 }
 </style>
